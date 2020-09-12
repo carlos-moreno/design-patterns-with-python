f304303e0d69becf9438ba55fd6a12f7ad82383a
Carlos Moreno <omorenodovale@gmail.com>
2020-09-11 22:07:05 -0300

    Python Factory Method implemantation example added.

---
+++ factory-method.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+
+
+class Section(ABC):
+    @abstractmethod
+    def describe(self):
+        pass
+
+
+class PersonalSection(Section):
+    def describe(self):
+        print("Personal Section")
+
+
+class AlbumSection(Section):
+    def describe(self):
+        print("Album Section")
+
+
+class PatentSection(Section):
+    def describe(self):
+        print("Patent Section")
+
+
+class PublicationSection(Section):
+    def describe(self):
+        print("Publication Section")
+
+
+class Profile(ABC):
+    def __init__(self):
+        self.sections = []
+        self.createProfile()
+
+    @abstractmethod
+    def createProfile(self):
+        pass
+
+    def getSections(self):
+        return self.sections
+
+    def addSections(self, section):
+        self.sections.append(section)
+
+
+class LinkedIn(Profile):
+    def createProfile(self):
+        self.addSections(PersonalSection())
+        self.addSections(PatentSection())
+        self.addSections(PublicationSection())
+
+
+class Facebook(Profile):
+    def createProfile(self):
+        self.addSections(PersonalSection())
+        self.addSections(AlbumSection())
+
+
+if __name__ == "__main__":
+    profile_type = input("Wich Profile you'd like to create? [LinkedIn or Facebook] ")
+    profile = eval(profile_type)()
+    print("Creating Profile..", type(profile).__name__)
+    print("Profile has sections --", profile.getSections())
